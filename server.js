9141c57960488fa97422c721fd3e1c1d1373c598
Dide Aydogan <dide1992@gmail.com>
2023-05-22 14:30:20 -0400

    created models and routes folders

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv")
-const cors = require("cors")
-const db = mongoose.connection
-dotenv.config()
+const mongoose = require("mongoose")
 
-const app = express()
 const PORT = process.env.PORT || 7000
+const authRoute = require("./routes/auth")
 
-app.get('/', (req, res) => {
-    res.send("hello")
-});
+dotenv.config()
+app.use(express.json())
+
+
+
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+})
+.then(console.log("Connected to MongoDB"))
+.catch((err)=> console.log(err));
+
+
+app.use("/api/auth", authRoute);
+
 
 app.listen(PORT, ()=> {
     console.log('app is running on port: ', PORT)
